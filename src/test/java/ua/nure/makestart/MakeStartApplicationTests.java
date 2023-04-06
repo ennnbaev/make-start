19aa740e0827c9f81f8a0e1e1d391dd0063ad5f0
Danylo Yenbaiev <ennnbaev@icloud.com>
2023-04-07 00:50:48 +0300

    MS-2 Added based class,pom.xml, .gitignore etc.

---
+++ src/test/java/ua/nure/makestart/MakeStartApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.makestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
