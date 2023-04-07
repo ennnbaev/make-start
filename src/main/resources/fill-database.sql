c7834bbf3044a2ddda60ea0f464676e2dec2787a
Karyna <karen.carter.29.12@gmail.com>
2023-04-07 16:09:55 +0300

    added liquibase properties, written init sql scripts

---
+++ src/main/resources/fill-database.sql
@@ -0,0 +1,27 @@
+INSERT INTO seniority (seniority_id, seniority_name)
+VALUES ('27ae3db1-00ce-43a1-a016-ce280c28c339', 'Trainee'),
+       ('fb3e95c6-6c54-4673-b9ce-89a1fc188201', 'Junior'),
+       ('77745df7-8308-4483-b3ad-c59aa994a154', 'Middle'),
+       ('f260a755-e454-4803-bf9f-de30de56679b', 'Senior');
+
+INSERT INTO _language (language_id, language_name)
+VALUES ('bd7894f9-3e2d-4847-adc4-1e1840910bb2', 'JavaScript'),
+       ('b91a0501-56a4-46d4-b184-9380764ddac3', 'Python'),
+       ('b40e2a0f-0755-4783-81f0-3d6c8d1db8fa', 'Go'),
+       ('284964c3-11d3-4315-9c78-03c19b0c78d7', 'Java'),
+       ('3e509303-d4fa-4861-9420-c7863ca3e42f', 'Kotlin'),
+       ('be9bf2f3-f5a7-4060-9100-b76e92d66349', 'PHP'),
+       ('698ecf90-6169-4e6f-b624-2584792b375f', 'C#'),
+       ('e56e2738-599e-4aa8-84e3-d2275b144b4d', 'Swift'),
+       ('bf5cb7f2-00b8-44f3-a854-63ac76f1ce76', 'R'),
+       ('5dd5cf93-1f92-45c7-950d-3af5e3cafe52', 'Ruby'),
+       ('74d0e1b7-7b3e-4b93-9bc5-99991d25dc65', 'C'),
+       ('4015c6e2-3fbd-4cc0-8326-6a3852ffb00f', 'C++'),
+       ('dc1695fc-b981-4c18-8c7c-921fde4cdca4', 'TypeScript'),
+       ('5e96aa72-7f12-4359-bb59-afa1d1922964', 'Scala'),
+       ('2db18009-e00d-4ae3-8fad-07d5e99146d8', 'Rust'),
+       ('239e23c8-decb-4b40-9352-f63df5b886ad', 'Perl'),
+       ('3b3f30d0-f502-4914-afe2-eeff4deb5e6d', 'Visual Basic'),
+       ('99d24cb4-4950-4f91-a072-88493f86616f', 'Delphi'),
+       ('79c5983f-8cd7-4592-898d-5595a80fccd0', 'Assembly language'),
+       ('5aab8155-e980-4741-99f5-d98fa69b6ca5', 'SQL');
---
+++ src/main/resources/start.sql
@@ -0,0 +1,97 @@
+CREATE DATABASE testDB;
+
+CREATE TABLE _language (
+    language_id varchar(255) NOT NULL,
+    language_name varchar(255) NOT NULL,
+    PRIMARY KEY (language_id)
+);
+
+CREATE TABLE seniority (
+    seniority_id varchar(255) NOT NULL,
+    seniority_name varchar(255) NOT NULL,
+    PRIMARY KEY (seniority_id)
+);
+
+CREATE TABLE team (
+    team_id varchar(255) NOT NULL,
+    team_name varchar(255) NOT NULL,
+    PRIMARY KEY (team_id)
+);
+
+CREATE TABLE project (
+    project_id varchar(255) NOT NULL,
+    project_name varchar(255) NOT NULL,
+    description varchar(255),
+    team_id varchar(255),
+    price float,
+    PRIMARY KEY (project_id)
+);
+
+CREATE TABLE _user (
+    user_id varchar(255) NOT NULL,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    age int, --???
+    cv_id varchar(255),
+    project_id varchar(255),
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE cv (
+    cv_id varchar(255) NOT NULL,
+    user_id varchar(255) NOT NULL,
+    seniority_id varchar(255) NOT NULL,
+    description varchar(255),
+    PRIMARY KEY (cv_id)
+);
+
+CREATE TABLE _position (
+    position_id varchar(255) NOT NULL,
+    project_id varchar(255) NOT NULL,
+    seniority_id varchar(255) NOT NULL,
+    position_status varchar(255),
+    user_id varchar(255),
+    PRIMARY KEY (position_id)
+);
+
+-- tables for many-to-many relationships
+CREATE TABLE language_position (
+    language_id varchar(255) NOT NULL,
+    position_id varchar(255) NOT NULL,
+    PRIMARY KEY (language_id, position_id)
+);
+
+CREATE TABLE language_cv (
+    language_id varchar(255) NOT NULL,
+    cv_id varchar(255) NOT NULL,
+    PRIMARY KEY (language_id, cv_id)
+);
+
+CREATE TABLE user_team (
+    user_id varchar(255) NOT NULL,
+    team_id varchar(255) NOT NULL,
+    PRIMARY KEY (user_id, team_id)
+);
+
+-- defining foreign keys
+ALTER TABLE project ADD CONSTRAINT FK_TEAM_PROJECT FOREIGN KEY (team_id) REFERENCES team(team_id);
+
+ALTER TABLE _user ADD CONSTRAINT FK_USER_CV FOREIGN KEY (cv_id) REFERENCES cv(cv_id);
+ALTER TABLE _user ADD CONSTRAINT FK_USER_PROJECT FOREIGN KEY (project_id) REFERENCES project(project_id);
+
+ALTER TABLE cv ADD CONSTRAINT FK_CV_USER FOREIGN KEY (user_id) REFERENCES _user(user_id);
+ALTER TABLE cv ADD CONSTRAINT FK_CV_SENIORITY FOREIGN KEY (seniority_id) REFERENCES seniority(seniority_id);
+
+ALTER TABLE _position ADD CONSTRAINT FK_POSITION_PROJECT FOREIGN KEY (project_id) REFERENCES project(project_id);
+ALTER TABLE _position ADD CONSTRAINT FK_POSITION_SENIORITY FOREIGN KEY (seniority_id) REFERENCES seniority(seniority_id);
+ALTER TABLE _position ADD CONSTRAINT FK_POSITION_USER FOREIGN KEY (user_id) REFERENCES _user(user_id);
+
+-- defining foreign keys for many-to-many relationships
+ALTER TABLE language_position ADD CONSTRAINT FK_LANG_POS_LANGUAGE FOREIGN KEY (language_id) REFERENCES _language(language_id);
+ALTER TABLE language_position ADD CONSTRAINT FK_LANG_POS_POSITION FOREIGN KEY (position_id) REFERENCES _position(position_id);
+
+ALTER TABLE language_cv ADD CONSTRAINT FK_LANG_CV_LANGUAGE FOREIGN KEY (language_id) REFERENCES _language(language_id);
+ALTER TABLE language_cv ADD CONSTRAINT FK_LANG_CV_CV FOREIGN KEY (cv_id) REFERENCES cv(cv_id);
+
+ALTER TABLE user_team ADD CONSTRAINT FK_USER_TEAM_USER FOREIGN KEY (user_id) REFERENCES _user(user_id);
+ALTER TABLE user_team ADD CONSTRAINT FK_USER_TEAM_TEAM FOREIGN KEY (team_id) REFERENCES team(team_id);
