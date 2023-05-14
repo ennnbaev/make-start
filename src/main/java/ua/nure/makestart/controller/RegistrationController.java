0c0d9139dc63d0cbaa867996b5f70425716ba9b9
DmitriyKuznetsov20 <dima.kuznetsov200224@gmail.com>
2023-05-14 11:36:16 +0300

    Start setup for registration page

--- src/main/java/ua/nure/makestart/controller/RegistrationController.java
+++ src/main/java/ua/nure/makestart/controller/RegistrationController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class RegistrationController {
-    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";}
+}
 //
 //    @Autowired
 //    private UserSevice userSevice;
