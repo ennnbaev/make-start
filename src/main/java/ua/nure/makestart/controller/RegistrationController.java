0a692a33fe82465899c193c7c426d8d4fa59abd8
Karyna <karen.carter.29.12@gmail.com>
2023-05-29 20:41:40 +0300

    added the same annotation to other controllers

--- src/main/java/ua/nure/makestart/controller/RegistrationController.java
+++ src/main/java/ua/nure/makestart/controller/RegistrationController.java
@@ -2,7 +2,9 @@ package ua.nure.makestart.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Controller
 public class RegistrationController {
 }
